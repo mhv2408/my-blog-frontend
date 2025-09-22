97175e4dda75a88e8724cedd5b9b55569558376d
Harsha Vardhan Mirthinti <mhv2408@gmail.com>
2025-09-21 23:40:25 -0700

    added Layout route for header and footer inclusion

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Main from './components/Main'
+import Main from './components/pages/Main'
 
 function App() {
 
   return (
     <div className="min-h-screen bg-white">
-      <Header />
-      <Main />
-      <Footer/>
-      
+      <Main />      
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,53 +3,49 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login from './components/Login.jsx'
-import Blogform from './components/Blogform.jsx'
+import Login from './components/pages/Login.jsx'
+import Blogform from './components/blog/Blogform.jsx'
 import ProtectedRoute from './components/ProtectedRoute.jsx'
-import Dashboard from './components/Dashboard.jsx'
-import EditBlogWrapper from './components/EditBlogWrapper.jsx'
-import BlogPostDetail from './components/BlogPost/BlogPostDetail.jsx'
+import Dashboard from './components/pages/Dashboard.jsx'
+import EditBlogWrapper from './components/blog/EditBlogWrapper.jsx'
+import BlogPostDetail from './components/blog/BlogPost/BlogPostDetail.jsx'
+import Layout from './components/layout/Layout.jsx'
 
 //define routes
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App/>
-  },
-  {
-    path: "/:slug",
-    element:<BlogPostDetail/>
-    ,
-  },
-  {
-    path: "/login",
-    element: <Login/>
+    element: <Layout />, 
+    children: [
+      { path: "/", element: <App /> },
+      { path: "/:slug", element: <BlogPostDetail /> },
+      { path: "/login", element: <Login /> },
+    ],
   },
   {
     path: "blog/dashboard",
-    element:(
+    element: (
       <ProtectedRoute>
         <Dashboard />
       </ProtectedRoute>
-    ) 
+    ),
   },
   {
     path: "blog/create",
-    element:(
+    element: (
       <ProtectedRoute>
-        <Blogform mode='create'/>
+        <Blogform mode="create" />
       </ProtectedRoute>
-    ) 
+    ),
   },
   {
     path: "blog/edit/:id",
-    element:(
+    element: (
       <ProtectedRoute>
         <EditBlogWrapper />
       </ProtectedRoute>
-    ) 
-  }
-])
+    ),
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
 
